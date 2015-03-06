74bb09fd164df7b5d1d10732746f9bad9700a891
Andy MacLean <andyjmaclean@gmail.com>
2015-03-06 14:45:20 +0100

    commit latest

--- app/helpers/component_helper.rb
+++ app/helpers/component_helper.rb
@@ -1,7 +1,23 @@
 module ComponentHelper
 
-  def ui_component(slug, properties={})
-    render "components/#{slug}", properties
+  def ui_data(cmp, properties)
+    properties
+  end
+  
+  
+  #def ui_component(slug, properties={})
+  #  render "components/#{slug}", properties
+  #end
+
+  
+  
+  def ui_component(slug, properties=JSON.parse({"europeana"=>"json"}.to_json))
+  #def ui_component(slug, properties={} )
+
+    
+    #render "components/#{slug}", ui_data(slug, properties)
+    render "components/#{slug}",  properties
   end
 
+  
 end
